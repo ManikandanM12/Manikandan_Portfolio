4a2b1a4fd8624dd8c5ef521acd7ff74ec59bfea8
ManikandanM12 <manikandanvmani2311@gmail.com>
2024-11-16 15:16:31 -0800

    Updated

--- assets/main.js
+++ assets/main.js
@@ -24,4 +24,10 @@ function linkAction() {
 }
 navlink.forEach(active=> active.addEventListener("click", linkAction));
 
+function darkTheme(){
+  document.body.classList.add("dark-theme")
+}
 
+function lightTheme(){
+  document.body.classList.remove("dark-theme")
+}
