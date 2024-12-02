b37fb522959085693658453aecba8e65c6898cc1
ManikandanM12 <manikandanvmani2311@gmail.com>
2024-12-02 07:30:43 -0800

    Portfolio updated

--- assets/main.js
+++ assets/main.js
@@ -15,19 +15,19 @@ showmenu("nav_icon", "nav_menu");
 //ACTIVE&REMOVE_ACTIVE
 
 const navlink = document.querySelectorAll(".nav_link");
-navlink.forEach(active=> active.classList.remove("active"))
+navlink.forEach((active) => active.classList.remove("active"));
 function linkAction() {
-  navlink.forEach(active => active.classList.remove("active"))
-  this.classList.add("active")
-  const navHide=document.getElementById("nav_menu")
-  navHide.classList.remove("show")
+  navlink.forEach((active) => active.classList.remove("active"));
+  this.classList.add("active");
+  const navHide = document.getElementById("nav_menu");
+  navHide.classList.remove("show");
 }
-navlink.forEach(active=> active.addEventListener("click", linkAction));
+navlink.forEach((active) => active.addEventListener("click", linkAction));
 
-function darkTheme(){
-  document.body.classList.add("dark-theme")
+function darkTheme() {
+  document.body.classList.add("dark-theme");
 }
 
-function lightTheme(){
-  document.body.classList.remove("dark-theme")
+function lightTheme() {
+  document.body.classList.remove("dark-theme");
 }
